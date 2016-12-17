109f1870da841dc861c0bd439e9bdb39ba10da0d
Kim, Christopher (CORP) <Chris.S.Kim@adp.com>
2016-12-16 21:57:27 -0800

    added popup

--- js/rally-data.js
+++ js/rally-data.js
@@ -1,30 +1,19 @@
-var RALLY_API_KEY = '_lipBuGakRCSLETr9nHB0XLFtMSFQU0LwtP2t3TupvvQ';
-var rally = require('rally');
-var queryUtils = rally.util.query;
-var Q = require('q');
+var rallyForm = document.getElementById('rallyData');
+var t = TrelloPowerUp.iframe();
 
-function apiSettings(options) {
-  return {
-    apiKey: RALLY_API_KEY,
-    server: 'https://rally1.rallydev.com'
-  }
+rallyForm.onsubmit = function(){
+
+  var id = t.card('name').split('—')[0].trim();
+  var data = {
+    _ref: id,
+    Actuals: document.getElementById('hours')
+  };
+
+var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+xmlhttp.open("PUT", "/api/defect");
+xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xmlhttp.onload = function(response){
+  document.getElementById('status').innerHTML = 'Saved to Rally';
+};
+xmlhttp.send(data);
 }
-
-var settings = apiSettings({ apiKey: options.apiKey });
-var restApi = rally(settings);
-
-restApi.update({
-    ref: '/defect/79914115024', //may be a ref ('/defect/1234') or an object with a _ref property
-    data: {
-        Name: 'My Updated Defect' //the data with which to update the specified object
-    },
-    fetch: ['FormattedID', 'Name'], //fields to fetch
-
-    requestOptions: {} //optional additional options to pass through to request
-}, function(error, result) {
-    if(error) {
-        console.log(error);
-    } else {
-        console.log(result.Object);
-    }
-});
--- server.js
+++ server.js
@@ -1,9 +1,61 @@
+var RALLY_API_KEY = '_lipBuGakRCSLETr9nHB0XLFtMSFQU0LwtP2t3TupvvQ';
 var express = require('express')
+var bodyParser = require('body-parser');
 var app = express()
-app.use(express.static('./'))
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var router = express.Router();
+var rally = require('rally');
+var Q = require('q');
+
+function apiSettings(options) {
+  return {
+    apiKey: RALLY_API_KEY,
+    server: 'https://rally1.rallydev.com'
+  }
+}
+
+var settings = apiSettings({
+  apiKey: RALLY_API_KEY
+});
+var restApi = rally(settings);
+app.use(express.static('./'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+router.use(function (req, res, next) {
+  // do logging
+  console.log('Something is happening.');
+  next();
+});
+router.route('/defect')
+  .put(function (req, res) {
+    restApi.update({
+      ref: {
+        _ref: req.body._ref
+      }, //may be a ref ('/defect/1234') or an object with a _ref property
+      data: {
+        Actuals: req.body.Actuals
+      },
+      fetch: ['FormattedID', 'Name', 'Actuals'], //fields to fetch
+
+      requestOptions: {} //optional additional options to pass through to request
+    }, function (error, result) {
+      if (error) {
+        console.log('error', error);
+      } else {
+        res.json(result.Object);
+      }
+    });
+
+
+
+  })
+  .get(function (req, res) {
+    res.json({
+      message: 'hooray! welcome to our api!'
+    });
+  });
+app.use('/api', router);
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log('Example app listening on port 80!')
